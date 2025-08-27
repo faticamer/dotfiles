9ad59397608acbde401b4427304ea1f9cea65a0c
faticamer <faticamer17@gmail.com>
2025-08-27 22:05:37 +0200

    Update .sh

--- installation-scripts/install-minimal.sh
+++ installation-scripts/install-minimal.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
-core_packages=("gcc" "make" "curl" "git" "tree" "xclip" "libevent-dev"
+#  __      __  _________.____
+# /  \    /  \/   _____/|    |
+# \   \/\/   /\_____  \ |    |
+#  \        / /        \|    |___
+#   \__/\  / /_______  /|_______ \
+#        \/          \/         \/
+
+core_packages=("gcc" "make" "curl" "git" "tree" "xclip" "bat" "ranger" "libevent-dev"
   "libncurses-dev" "build-essential" "bison" "pkg-config")
 
 ensure_is_installed() {
   type "$1" >/dev/null 2>&1 || {
-    echo "$1" >>/tmp/install-minimal-logs.txt
     echo "####### Installing missing package: $1 #######"
     yes | sudo apt-get install "$1"
   }
 }
 
 ensure_core_packages_are_installed() {
-  for ((i = 0; i <= ${#core_packages[@]}; i++)); do
+  for ((i = 0; i < ${#core_packages[@]}; i++)); do
     ensure_is_installed "${core_packages[i]}"
   done
+}
+
+typecheck() {
+  type "$1" >/dev/null 2>&1 || {
+    echo "####### Binary not found: $1 #######"
+    echo -e "Failed to execute:\n$1\n" >>/tmp/install-minimal-logs.txt
+  }
+}
+
+add_alias() {
+  # Note: if bat is installed thru apt, setting an alias
+  # is not enough for fzf to find the binary, so the original package name
+  # needs to be specified --batcat
+  echo -e "\nalias anvim='nvim \$(fzf -m --preview=\"batcat --color=always --style=numbers --line-range=:500 {}\")'" \
+    >>~/.bashrc
+
+  echo -e '\nalias bat="batcat"' \
+    >>~/.bashrc
+
+  source ~/.bashrc
 }
 
 # -- Fetch repository package updates
@@ -29,9 +55,9 @@ ensure_core_packages_are_installed
 echo "####### Setting up TMUX ... #######"
 curl -L https://github.com/tmux/tmux/releases/download/3.5a/tmux-3.5a.tar.gz >~/tmux.tar.gz
 sudo tar -C $HOME -xzf ~/tmux.tar.gz
-cd ~/tmux-3.5a && sudo ./configure && make
+cd ~/tmux-3.5a && sudo ./configure && sudo make
 sudo make install
-ensure_is_installed tmux
+typecheck tmux
 cd || exit
 # --
 
@@ -43,7 +69,7 @@ sudo cp nvim-linux-x86_64.appimage /usr/bin/nvim
 sudo chmod 775 /usr/bin/nvim
 rm nvim-linux-x86_64.appimage
 cd || exit
-ensure_is_installed nvim
+typecheck nvim
 # --
 
 # -- Go Installation
@@ -54,33 +80,33 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >>~/.profile
 source ~/.profile
-ensure_is_installed go
+typecheck go
 # --
 
 # Grab fd-find
 echo "####### Installing fdfind #######"
 yes | sudo apt-get install fd-find &&
-  ensure_is_installed fdfind
+  typecheck fdfind
 
 # Grab fzf
 echo "####### Installing fzf #######"
 yes | sudo apt-get install fzf &&
-  ensure_is_installed fzf
+  typecheck fzf
 
 # Grab python3
 echo "####### Installing Python #######"
 yes | sudo apt-get install python3 &&
-  ensure_is_installed python3
+  typecheck python3
 
 # Grab ripgrep
 echo "####### Installing Ripgrep #######"
 sudo apt-get install ripgrep &&
-  ensure_is_installed rg
+  typecheck rg
 
 # Grab Lazygit
 echo "####### Installing Lazygit #######"
 go install github.com/jesseduffield/lazygit@latest &&
-  ensure_is_installed lazygit
+  typecheck lazygit
 
 # Grab luarocks
 echo "####### Installing Luarocks #######"
@@ -90,6 +116,9 @@ yes | sudo apt-get install luarocks
 # git config --global user.name $user
 # git config --global user.email $email
 # --
+
+# Add some aliases
+add_alias
 
 # Fetch TPM
 echo " ####### Setting up TPM #######"
