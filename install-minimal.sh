374216bb6c5a1875591d10b822b97ff123bc5493
faticamer <faticamer17@gmail.com>
2025-08-27 12:47:32 +0200

    Update script

--- install-minimal.sh
+++ install-minimal.sh
@@ -3,10 +3,6 @@
 core_packages=("gcc" "make" "curl" "git" "tree" "xclip" "libevent-dev"
   "libncurses-dev" "build-essential" "bison" "pkg-config")
 
-gotoHome() {
-  cd || exit
-}
-
 ensure_is_installed() {
   type "$1" >/dev/null 2>&1 || {
     echo "$1" >>/tmp/install-minimal-logs.txt
@@ -36,7 +32,7 @@ sudo tar -C $HOME -xzf ~/tmux.tar.gz
 cd ~/tmux-3.5a && sudo ./configure && make
 sudo make install
 ensure_is_installed tmux
-gotoHome
+cd || exit
 # --
 
 # -- Grab the latest NeoVim AppImage...
@@ -46,13 +42,13 @@ cd ~/Downloads || exit
 sudo cp nvim-linux-x86_64.appimage /usr/bin/nvim
 sudo chmod 775 /usr/bin/nvim
 rm nvim-linux-x86_64.appimage
-gotoHome
+cd || exit
 ensure_is_installed nvim
 # --
 
 # -- Go Installation
 echo "####### Installing Go... #######"
-gotoHome
+cd || exit
 curl -LOk https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
@@ -68,35 +64,34 @@ yes | sudo apt-get install fd-find &&
 
 # Grab fzf
 echo "####### Installing fzf #######"
-yes | sudo apt-get install fzf
-ensure_is_installed fzf
+yes | sudo apt-get install fzf &&
+  ensure_is_installed fzf
 
 # Grab python3
 echo "####### Installing Python #######"
-yes | sudo apt-get install python3
-ensure_is_installed python3
+yes | sudo apt-get install python3 &&
+  ensure_is_installed python3
 
 # Grab ripgrep
 echo "####### Installing Ripgrep #######"
-sudo apt-get install ripgrep
-ensure_is_installed rg
+sudo apt-get install ripgrep &&
+  ensure_is_installed rg
 
 # Grab Lazygit
 echo "####### Installing Lazygit #######"
-go install github.com/jesseduffield/lazygit@latest
-ensure_is_installed lazygit
+go install github.com/jesseduffield/lazygit@latest &&
+  ensure_is_installed lazygit
 
 # Grab luarocks
 echo "####### Installing Luarocks #######"
 yes | sudo apt-get install luarocks
 
 # -- Setup Git credentials
-git config --global user.name $user
-git config --global user.email $email
+# git config --global user.name $user
+# git config --global user.email $email
 # --
 
-# Fetch dotfiles & TPM
-# git clone https://$user:$pat@github.com/$user/$repo
+# Fetch TPM
 echo " ####### Setting up TPM #######"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # --
