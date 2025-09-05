364db7658534438685962682d686f6e89e80d683
faticamer <faticamer17@gmail.com>
2025-09-05 22:48:18 +0200

    Setup md plugin
    Setup & configure markdown-preview.nvim plugin

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,14 @@
 return {
+  {
     "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && npm install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
-    ft = { "markdown" },
+    config = function()
+      vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
+      vim.keymap.set("n", "<Leader>ms", "<Plug>MarkdownPreviewStop", { desc = "Markdown Preview - Stop" })
+      vim.keymap.set("n", "<Leader>mt", "<Plug>MarkdownPreviewToggle", { desc = "Toggle" })
+    end,
+  },
 }
